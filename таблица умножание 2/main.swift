1a43063703b8d43d7f368915a62f1be6139351a3
bombag456 <bombag456@gmail.com>
2022-09-14 16:40:23 +0600

    вц

--- таблица умножание 2/main.swift
+++ таблица умножание 2/main.swift
@@ -14,6 +14,13 @@ for nadik in 1...10{
     print("1*\(nadik) = \(nadik*g)")
 }
 
-
+func printName(){
+    print("about myself")
+}
+print("My names-Akbar")
+print("surname-Roziev")
+print("Age-18")
+print("My number-0700632230")
+print("Iam born in Oh")
 
 
