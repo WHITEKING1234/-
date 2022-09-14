e1da9c706f5dce08f0a35564134bdd3551cf4c3b
bombag456 <bombag456@gmail.com>
2022-09-14 16:42:52 +0600

    выввйв

--- таблица умножание 2/main.swift
+++ таблица умножание 2/main.swift
@@ -24,3 +24,29 @@ print("My number-0700632230")
 print("Iam born in Oh")
 
 
+
+let a :Int = 1199
+var b :Int = 2000
+print(a+b)
+
+
+let dommle :Double = 1.23
+var domle :Double = 3.48
+print(domle * domle)
+
+let adc :Bool = true
+var ad2 :Bool = false
+print (adc)
+
+let DA :Character = "J"
+var dw :Character = "b"
+
+let stie :String = "NAD"
+var slet :String = "ik"
+print(stie+slet)
+
+
+print(a+b)
+var c = a+b
+
+print(c)
